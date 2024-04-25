5c0898684c89e03e6c9fea9bd310d4dae158478f
mkao mwakuini <mkaoibrahim@gmail,com>
2024-04-25 22:12:10 +0300

    Jetpack Components and configuration changes

--- app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
@@ -1,6 +1,8 @@
 package com.mkao.JetpackComponents
 
+import android.annotation.SuppressLint
 import android.widget.Toast
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
@@ -8,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun CheckBoxesM3 () {
     var checked by mutableStateOf(true)
@@ -39,4 +43,33 @@ fun CheckBoxesM3 () {
 @Composable
 fun CheckedPreview(){
     CheckBoxesM3()
+}
+
+@Composable
+fun ListofCheckBoxes() {
+    val toppings: List<String> = listOf("BMW M3", "BMW M5", "BMW E5", "BMW M6")
+
+    val context = LocalContext.current.applicationContext
+
+    Column(horizontalAlignment = Alignment.Start) {
+        toppings.forEach { toppingsName ->
+            var checked by remember {
+                mutableStateOf(true)
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(
+                    checked = checked,
+                    onCheckedChange = { checkedStatus ->
+                        checked = checkedStatus
+                    })
+                Toast.makeText(context,"You Checked $toppingsName",Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
+@Preview
+@Composable
+fun ListChecksPreview(){
+    ListofCheckBoxes()
 }
