bc47309e30288b93eff8bc3e733f0089c2ee576b
mkaocodes <149311528+Yesim02@users.noreply.github.com>
2024-04-28 13:15:56 +0300

    Build Changes and Jetpack Compose Components added

--- app/src/main/java/com/mkao/JetpackComponents/Button/Button.kt
+++ app/src/main/java/com/mkao/JetpackComponents/Button/Button.kt
@@ -1,4 +1,13 @@
 package com.mkao.JetpackComponents.Button
 
-class Button {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ButtonM3() {
+}
+@Preview
+@Composable
+fun ButtonPreview(){
+    ButtonM3()
 }
