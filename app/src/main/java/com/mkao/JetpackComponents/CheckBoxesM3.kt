866d0b3a4380a6000d522281c5861f38941bcbe0
mkaocodes <149311528+Yesim02@users.noreply.github.com>
2024-05-13 14:26:12 +0300

    Build Changes and Jetpack Compose Components added

--- app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
@@ -1,4 +1,36 @@
 package com.mkao.JetpackComponents
 
-class CheckBoxesM3 {
+import android.widget.Toast
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CheckBoxesM3 () {
+    var checked by mutableStateOf(true)
+    val ctx = LocalContext.current.applicationContext
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checkedStatus ->
+                checked = checkedStatus
+                Toast.makeText(ctx, "You Choose : $checkedStatus", Toast.LENGTH_LONG).show()
+            }
+        )
+        Text(text = "BMW")
+    }
+}
+@Preview
+@Composable
+fun CheckedPreview(){
+    CheckBoxesM3()
 }
