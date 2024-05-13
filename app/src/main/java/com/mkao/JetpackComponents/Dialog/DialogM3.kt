3f19dbcae9e94fcf12c134cdc9baeccf6d74d7a0
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-13 23:38:06 +0300

    Jetpack Components -custom Dialog

--- app/src/main/java/com/mkao/JetpackComponents/Dialog/DialogM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/Dialog/DialogM3.kt
@@ -1,11 +1,17 @@
 package com.mkao.JetpackComponents.Dialog
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -13,7 +19,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
 @Composable
 fun DialogM3() {
@@ -57,4 +67,49 @@ fun DialogM3() {
 @Composable
 fun AlerDialogPreview(){
     DialogM3()
+}
+
+@Composable
+fun DialogM3Custom() {
+    var operationOpenDialog by remember {
+        mutableStateOf(false)}
+
+    IconButton(
+        onClick = { operationOpenDialog = true}) {
+        Icon(
+            imageVector = Icons.Outlined.Build,
+            contentDescription = null
+        )
+
+    }
+    if (operationOpenDialog){
+        Dialog(
+            onDismissRequest = { operationOpenDialog = false }) {
+            Surface {
+                Column(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                    Text(
+                        text = "Congratulations",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                    Text(
+                        text = "You won the competitions",
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "View Results"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CustomDialog(){
+    DialogM3Custom()
 }
