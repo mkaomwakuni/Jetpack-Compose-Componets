1b8a133d86eecef9a793084bb7f0b4408a8d351d
mkaocodes <149311528+Yesim02@users.noreply.github.com>
2024-05-13 14:35:40 +0300

    Build Changes and Jetpack Compose Components added

--- app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/CheckBoxesM3.kt
@@ -3,6 +3,7 @@ package com.mkao.JetpackComponents
 import android.widget.Toast
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -24,7 +26,11 @@ fun CheckBoxesM3 () {
             onCheckedChange = { checkedStatus ->
                 checked = checkedStatus
                 Toast.makeText(ctx, "You Choose : $checkedStatus", Toast.LENGTH_LONG).show()
-            }
+            },
+            colors = CheckboxDefaults.colors (
+                checkedColor = Color.Green,
+                uncheckedColor = Color.LightGray
+            )
         )
         Text(text = "BMW")
     }
