6a4d22f1dfe34da1492d2ef68bc7da4cc7fff9e0
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-14 12:04:18 +0300

    Jetpack Compose Components -Sliders( Range and Slider)

--- app/src/main/java/com/mkao/JetpackComponents/SliderM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/SliderM3.kt
@@ -1,4 +1,85 @@
 package com.mkao.JetpackComponents
 
-class SliderM3 {
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun SliderM3() {
+    var sliderPosition by remember {
+        mutableStateOf(0f)
+    }
+    Column (modifier = Modifier.padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = sliderPosition.toString())
+    }
+    Slider(
+        value = sliderPosition,
+        onValueChange = {sliderPosition= it},
+        steps = 10,
+        valueRange = 0f..5f,
+        onValueChangeFinished = {
+            //ViewModel.updateSelectedSliderValue(sliderPosition)
+        },
+        thumb = {
+            Icon(
+                imageVector = Icons.Outlined.ShoppingCart ,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+                tint = Color.DarkGray)
+        }
+
+    )
 }
+
+
+@Composable
+fun PriceRangeSlider() {
+    var sliderPosition by remember {
+        mutableStateOf(10f..100f)
+    }
+    Column (
+        modifier = Modifier.padding(horizontal = 10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+        Text(text = sliderPosition.toString())
+        RangeSlider(
+            value = sliderPosition,
+            onValueChange = {sliderPosition = it},
+            valueRange = 0f..100f)
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun RangeSliderPreviews(){
+    PriceRangeSlider()
+}
+@Preview(showBackground = true)
+@Composable
+fun SliderPreview(){
+  SliderM3( )
+}
+
