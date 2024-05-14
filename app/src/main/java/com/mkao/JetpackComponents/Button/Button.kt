4c702187b7c2601ef4aafa51125848daabadbfd8
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-14 14:15:08 +0300

    Jetpack Compose Components - Button

--- app/src/main/java/com/mkao/JetpackComponents/Button/Button.kt
+++ app/src/main/java/com/mkao/JetpackComponents/Button/Button.kt
@@ -1,10 +1,24 @@
 package com.mkao.JetpackComponents.Button
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ButtonM3() {
+Button(
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(60.dp),
+    onClick = { /*TODO*/ }) {
+    Text(text = "Submit")
+    
+}
 }
 @Preview
 @Composable
