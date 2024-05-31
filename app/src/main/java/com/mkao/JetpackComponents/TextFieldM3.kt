dcc2e184dfc52b893a533a04c2cfe7a9fc95500f
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-31 15:35:45 +0300

    Jetpack Compose Components -Sliders( FAB and NavigationBar)

--- app/src/main/java/com/mkao/JetpackComponents/TextFieldM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/TextFieldM3.kt
@@ -1,4 +1,57 @@
-package com.mkao.JetpackComponents
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
-class TextFieldM3 {
+@Composable
+fun TextFieldM3(
+    label: String = "Search",
+    placeholder: String = "Search Bar",
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
+) {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText },
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        leadingIcon = leadingIcon,
+        trailingIcon = {
+            if (text.isEmpty()) {
+                IconButton(onClick = { text = "" }) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear Icon"
+                    )
+                }
+            } else {
+                trailingIcon?.invoke()
+            }
+        },
+        modifier = modifier,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+    )
+}
+
+@Preview
+@Composable
+fun TextFieldM3Preview(){
+    TextFieldM3()
 }
