8b59d64d0b98397a5114b480f5126b4fb5a64efe
mkaomwakuni <mkaoibrahim@gmail.com>
2024-05-31 16:05:28 +0300

    Switch Added

--- app/src/main/java/com/mkao/JetpackComponents/SwitchM3.kt
+++ app/src/main/java/com/mkao/JetpackComponents/SwitchM3.kt
@@ -1,4 +1,62 @@
 package com.mkao.JetpackComponents
 
-class SwitchM3 {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SwitchM3(
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Switch(
+        checked = isChecked,
+        onCheckedChange = onCheckedChange,
+        modifier = modifier
+    )
 }
+@Composable
+fun SwitchScreen() {
+    var isChecked by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SwitchM3(
+            isChecked = isChecked,
+            onCheckedChange = { isChecked = it },
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = if (isChecked) "Switch is ON" else "Switch is OFF",
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun SwitchScreenPreview() {
+    MaterialTheme {
+        SwitchScreen()
+    }
+}
+
